95822228e9c0bc70f629b1c65dadcb161d743ec8
Luna <87461596+LunaXCBN@users.noreply.github.com>
2023-10-15 13:23:12 +0300

    finally fix the fonts and remove steam api key

--- main.js
+++ main.js
@@ -6,15 +6,3 @@ function age() {
     return ageInYears + " years old";
 }
 document.getElementById("age").innerHTML = age();
-
-async function getStatus(url = "", data = {}) {
-    var opts = {
-        headers: {
-            "mode":"no-cors"
-        }
-    }
-    fetch("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=E88C8F550DFA5B17B4156616A25C6893&format=json&steamids=76561198148554560")
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .then(error => console.error('Error:', error));
-}
